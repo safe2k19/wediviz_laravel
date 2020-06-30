1ed8e23b2f6332876a47dabba9027284df733d47
Dima <zyumalin@bk.ru>
2020-06-30 18:43:51 +0300

    made a list of applications

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,4 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('{clients}') }}'><i class='nav-icon la la-question'></i> {Clients}s</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('popup') }}'><i class='nav-icon la la-question'></i> Popups</a></li>
+<!-- <li class='nav-item'><a class='nav-link' href='{{ backpack_url('{clients}') }}'><i class='nav-icon la la-question'></i> {Clients}s</a></li> -->
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('popup') }}'><i class='nav-icon la la-question'></i> Список заявок</a></li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,5 +14,5 @@ Route::group([
     ],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-    Route::crud('popup', 'PopupController');
+    Route::crud('popup', 'PopupCrudController');
 }); // this should be the absolute last line of this file
