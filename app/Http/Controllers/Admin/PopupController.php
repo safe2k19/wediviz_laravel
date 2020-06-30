304464b393f065d3f4c943fc50c8b50991cfdd66
Dima <zyumalin@bk.ru>
2020-06-30 17:33:10 +0300

    -

---
+++ app/Http/Controllers/Admin/PopupController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PopupController extends Controller
+{
+    public function popup()
+    {
+        return 'popup';
+    }
+}
